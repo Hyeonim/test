0fbc562be5308933aeb8891445156fd47b9640f1
Hyeonim <abc105484@naver.com>
2026-03-10 11:08:47 +0900

    새로운 브런치 커밋

--- src/test.java
+++ src/test.java
@@ -1,15 +1,9 @@
-import java.io.*;
-import java.nio.charset.StandardCharsets;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.zip.*;
-
 public class test {
     public static void main(String[] args)
     {
 
         String  a ="s";
 
-        System.out.println(a);
+        System.out.println("새작업");
     }
 }
